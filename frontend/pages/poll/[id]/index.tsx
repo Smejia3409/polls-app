027aca38fc549f0f0a8f7760b19189fbd99f3379
Sandro Mejia <sandromejia3409@gmail.com>
2023-02-02 23:54:44 -0500

    added basic poll stats to nested route page

--- frontend/pages/poll/[id]/index.tsx
+++ frontend/pages/poll/[id]/index.tsx
@@ -1,14 +1,46 @@
 import { useRouter } from "next/router";
-import React from "react";
-import { IPoll } from "../../../jsfiles/interfaces";
+import React, { useState } from "react";
+import { IPoll, IpollAns } from "../../../jsfiles/interfaces";
 import axios from "axios";
+import { Card } from "react-bootstrap";
+import PollCard from "../../../components/PollCard";
 
-const poll = (data: [IPoll]) => {
-  console.log(data);
+const poll = (data: any) => {
   const router = useRouter();
   const { id } = router.query;
 
-  return <div>This is poll page {id}</div>;
+  let poll: IPoll = data.data[0];
+  // if (data.id) {
+  //   console.log(1);
+  // } else {
+  //   console.log(0);
+  // }
+  const [voteCount, setVoteCount] = useState<number>(0);
+  let num = 0;
+  poll.answers.forEach((count) => {
+    num = num + count.count;
+  });
+
+  return (
+    <div className="container">
+      <p> This is poll page {id}</p>
+      <p>{num}</p>
+      <Card>
+        <Card.Body>
+          <Card.Title>{poll.question}</Card.Title>
+          {poll.answers.map((question: IpollAns) => {
+            return (
+              <>
+                <Card.Text>{question.answer}</Card.Text>
+                <Card.Text>{(question.count / num) * 100}%</Card.Text>
+              </>
+            );
+          })}
+        </Card.Body>
+      </Card>
+      {/* <PollCard key={poll.id} list={data.data} /> */}
+    </div>
+  );
 };
 
 export async function getServerSideProps(context: any) {
